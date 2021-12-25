3ea6cd540323308bd3ae8d7928c917e8688102c8
neko341xyz <neko341xyz@gmail.com>
2021-12-25 23:25:13 +0900

    fix: eslint

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,22 +1,29 @@
-import React, {FC,} from "react";
-import {Outlet} from "react-router-dom";
-import {SideMenu} from "../models/Sidemenu";
-import {BaseHeader} from "../models/Header";
+import React, { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+import { SideMenu } from '../models/Sidemenu';
+import { BaseHeader } from '../models/Header';
 
+function BaseFooter() {
+  return (
+    <div className="flex flex-item justify-center bg-gray-100 h-64 z-10">
+      <span>フッター</span>
+    </div>
+  );
+}
 
 export const DefaultLayout: FC = () => {
-    return (
-        <>
-            <div className="flex flex-col bg-gray-100 h-full">
-                <BaseHeader/>
-                <div className="flex flex-item">
-                    <SideMenu/>
-                    <main className=" container w-screen mx-auto flex flex-col  ">
-                        <Outlet/>
-                    </main>
-                </div>
-                <div className="flex flex-item justify-center bg-gray-400"><span>フッター</span></div>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="flex flex-col bg-gray-100 h-full">
+        <BaseHeader />
+        <div className="flex flex-item">
+          <SideMenu />
+          <main className="container w-screen mx-auto flex flex-col  ">
+            <Outlet />
+          </main>
+        </div>
+        <BaseFooter />
+      </div>
+    </>
+  );
+};
