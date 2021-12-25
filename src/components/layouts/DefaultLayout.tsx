6dc99540034c92774a81223220ba34b649a908ba
neko341xyz <neko341xyz@gmail.com>
2021-12-25 20:52:55 +0900

    refactor: header

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,13 +1,14 @@
 import React, {FC,} from "react";
 import {Outlet} from "react-router-dom";
 import {SideMenu} from "../ui/Sidemenu";
+import {BaseHeader} from "../ui/Header";
 
 
 export const DefaultLayout: FC = () => {
     return (
         <>
-            <div className="flex flex-col bg-gray-100 h-full max-w-screen">
-                <div className="flex flex-item justify-center bg-gray-400"><span>ヘッダー</span></div>
+            <div className="flex flex-col bg-gray-100 h-full">
+                <BaseHeader/>
                 <div className="flex flex-item">
                     <SideMenu/>
                     <main className=" container w-screen mx-auto flex flex-col  ">
---
+++ src/components/ui/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const BaseHeader = () => {
+    return (<div className="flex flex-item justify-center align-middle bg-gray-100 shadow shadow-lg h-16">
+        <div className="flex flex-row">
+            <div className="flex flex-item align-middle">
+                <span className="text-3xl self-center">ヘッダー</span>
+            </div>
+        </div>
+    </div>);
+}
