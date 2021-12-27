1702b90637678dc3d38df0cd2dd0212332aa6b6a
neko341xyz <neko341xyz@gmail.com>
2021-12-27 23:26:02 +0900

    fix: z-index

--- src/components/models/Header/index.tsx
+++ src/components/models/Header/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const BaseHeader = () => {
   return (
         <div className="flex justify-center items-center bg-gray-900 h-16 shadow-2xl drop-shadow">
-          <span className="text-3xl text-gray-100">ヘッダー</span>
+          <span className="text-3xl text-gray-100 z-10">ヘッダー</span>
         </div>
   );
 };
