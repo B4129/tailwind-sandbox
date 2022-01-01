7462a5229f8099ad0bbc500db89e33a6f1178013
B4129 <B4129@oic.jp>
2022-01-01 14:30:24 +0900

    add:下線の追加

--- src/components/ui/Labels/UnderLabel/index.tsx
+++ src/components/ui/Labels/UnderLabel/index.tsx
@@ -1,11 +1,10 @@
 import React, { FC } from 'react';
 export const UnderLabel: FC = () => {
-    return (
-        <>
-            <p
-                className="b">
-            aaa
-            </p>
-        </>
-    );
+  return (
+    <>
+      <div className="  ">
+        <span className="hover:border-b hover:border-b-blue-900 text-3xl transition-all">aaaa</span>
+      </div>
+    </>
+  );
 };
